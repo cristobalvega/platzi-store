83189c13ff8943653acddc82041cdb818a617c1b
cristobalvega <cristobalvega96@gmail.com>
2020-03-26 23:39:38 -0600

    Creando el formulario de productos

--- src/app/admin/components/form-product/form-product.component.ts
+++ src/app/admin/components/form-product/form-product.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form-product',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormProductComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder
+  ) {
+    this.buildForm();
+  }
 
   ngOnInit(): void {
+  }
+
+  private buildForm() {
+    this.form = this.formBuilder.group({
+      id: ['', [Validators.required]],
+      title: ['', [Validators.required]],
+      price: ['', [Validators.required]],
+      image: [''],
+      description: ['', [Validators.required]],
+    });
   }
 
 }
