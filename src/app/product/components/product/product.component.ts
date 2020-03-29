d3b99355502a688f484b0687d7929f0ba73a3321
cristobalvega <cristobalvega96@gmail.com>
2020-03-28 18:00:09 -0600

    lint: se eliminan variables inutilizables

--- src/app/product/components/product/product.component.ts
+++ src/app/product/components/product/product.component.ts
@@ -3,8 +3,6 @@ import {
     Input,
     Output,
     EventEmitter,
-    OnChanges,
-    SimpleChanges,
     OnInit,
     DoCheck,
     OnDestroy } from '@angular/core';
@@ -16,12 +14,10 @@ import { CartService } from '../../../core/services/cart.service';
     templateUrl: './product.component.html',
     styleUrls: ['./product.component.scss'],
 })
-export class ProductComponent implements OnInit, DoCheck, OnDestroy { // OnChanges, OnInit {
+export class ProductComponent implements OnInit, DoCheck, OnDestroy {
 
     @Input() product: Product;
     @Output() productClicked: EventEmitter<any> = new EventEmitter();
-
-    today = new Date();
 
     constructor(
       private cartService: CartService
@@ -44,7 +40,6 @@ export class ProductComponent implements OnInit, DoCheck, OnDestroy { // OnChang
     addCart() {
         console.log('Añadir al carrito');
         this.cartService.addCart(this.product);
-        // this.productClicked.emit(this.product.id);
     }
 
 }
